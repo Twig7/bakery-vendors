89138aec8e2f7a0e32b701bcc665fcd2c67a46dd
Amber W <amoothenielson@gmail.com>
2022-02-26 21:22:42 -0800

    Try to troubleshoot tests, still giving me errors over and over. Cross referenced with last testing done successfully and nothing seems different.

--- VendorOrders.Tests/ModelTests/VendorTests.cs
+++ VendorOrders.Tests/ModelTests/VendorTests.cs
@@ -1,4 +1,19 @@
 // using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using VendorOrder.Models;
-using System;
+// using System.Collections.Generic;
+// using VendorOrder.Models;
+// using System;
+
+// namespace VendorOrder.Tests
+// {
+//   [TestClass]
+//   public class VendorTests
+//   {
+//     [TestMethod]
+//     public void GetName_ReturnsName_String()
+//     {
+//       string name = "Test Name";
+//       string info = "Test Info";
+//       Vendor newVendor = new Vendor(name, info);
+//     }
+//   }
+// }
